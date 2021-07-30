2efe8015e174f18ebb572dedacac80cf3b17a0ed
royjan <roy@jan.co.il>
2021-07-31 00:45:20 +0300

    added files

--- main.c
+++ main.c
@@ -14,7 +14,8 @@
 int new_main(int argc, char *argv[]);
 void new_slave(char *seq1, char *seq2);
 void new_master(char **seq1, char **seq2, char **procedure);
-void checkOneCase(int my_rank, int batch, int x, Scores *scores_each_t, char *seq1, char *seq2, int size_seq2);
+void checkOneCase(int my_rank, int batch, int x, Scores *scores_each_t,
+		char *seq1, char *seq2, int size_seq2);
 //
 
 void createScoreType(MPI_Datatype *scoreMpiType);
@@ -94,10 +95,10 @@ int checkSemiOrSemiConservativeGroups1(char c1, char c2, const char *group[],
 			if (found_c1 == '-' && c1 == group[i][j]) {
 				found_c1 = c1;
 			}
-			if (found_c2 == '-'  && c2 == group[i][j]) {
+			if (found_c2 == '-' && c2 == group[i][j]) {
 				found_c2 = c2;
 			}
-			if (found_c1 != '-'  && found_c2 != '-' ) {
+			if (found_c1 != '-' && found_c2 != '-') {
 				return 1;
 
 			}
@@ -115,6 +116,7 @@ int calcMaxOffsetForEachSeq(char *seq1, char *seq2) {
 	maxOffsetEachSeq2 = abs(size_seq1 - size_seq2);
 	return maxOffsetEachSeq2;
 }
+
 void addMutant(char *seq1, char *seq2, int size_seq2, char *new_seq2) {
 
 	const char *cons_Groupss[9] = { "NDEQ", "MILV", "FYM", "NEQK", "QHRK", "HY",
@@ -192,67 +194,6 @@ void addMutantsEachSeq(char *seq2, int size_seq2, char **new_seq2, int *i,
 	}
 }
 
-void compareSequence(char *seq1, char *seq2, Counts *counts, Weights weights,
-		Scores *scores) {
-	int my_rank;
-	MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
-	int j;
-	int size_seq1;
-	int size_seq2;
-	char *new_seq2;
-	int size_new_seq;
-	int mute_loc = 0;
-	int offset = 0;
-	scores->score = 0.0;
-	scores->mute_loc = 0;
-	scores->offset = 0;
-	size_seq1 = getSize1(seq1);
-	double maxScore = -INFINITY;
-	size_seq2 = getSize1(seq2);
-	size_new_seq = size_seq2 + 1;
-	if (size_seq1 == size_seq2) {
-		addMutant(seq1, seq2, size_seq2, new_seq2);
-		//printf("%s\n",new_seq2);
-		scores->score = compareSameLenSequences(weights, seq1, new_seq2,
-				size_seq2);
-		//printf("%1.3f\n", scores->score);
-		//addMutant(seq1,seq2,size_seq2,new_seq2);
-		//	printf("%s\n",new_seq2);
-
-		/*
-		 for (j = 0; j < size_new_seq; j++) {
-		 addMutantsEachSeq(seq2, size_seq2, &new_seq2, &j, &scores);
-		 scores->score = compareSameLenSequences(weights, seq1, new_seq2,
-		 size_seq2);
-		 if (scores->score > maxScore) {
-		 maxScore = scores->score;
-		 }
-		 }*/
-	} else {
-		addMutant(seq1, seq2, size_seq2, new_seq2);
-		//printf("%s\n",new_seq2);
-		scores->score = compareDiffrentLenSequences(weights, seq1, new_seq2,
-				size_seq2, &scores);
-		//printf("%1.3f\n", scores->score);
-
-		/*
-		 for (j = 0; j < size_new_seq; j++)
-		 addMutantsEachSeq(seq2, size_seq2, &new_seq2, &j, &scores);
-		 scores->score = compareDiffrentLenSequences(weights, seq1, new_seq2,
-		 size_seq2, &scores);
-		 if (scores->score > maxScore) {
-		 maxScore = scores->score;
-		 mute_loc = scores->mute_loc;
-		 offset = scores->offset;
-		 }
-		 */
-	}
-//	scores->score = maxScore;
-	//scores->mute_loc = mute_loc;
-	//scores->offset = offset;
-}
-
-//make this set offset to worker 
 double compareDiffrentLenSequences(Weights weights, char *seq1, char *seq2,
 		int size_seq2, Scores **scores) {
 	int my_rank;
@@ -283,7 +224,7 @@ double compareDiffrentLenSequences(Weights weights, char *seq1, char *seq2,
 		for (i = 0; i < maxOffset + 1; i++) {
 			double score = 0.0;
 			substring(seq1, sub, i + 1, size_seq2);
-			score = compareSameLenSequences(weights, sub, seq2, size_seq2);
+			score = 2.0;//compareSameLenSequences(weights, sub, seq2, size_seq2);
 			if (score > max_scores_each_t[tid]) {
 				max_scores_each_t[tid] = score;
 				offset_each_t[tid] = i;
@@ -299,27 +240,6 @@ double compareDiffrentLenSequences(Weights weights, char *seq1, char *seq2,
 	}
 
 	return max_score;
-}
-
-//this is the comperation , the import one 
-double compareSameLenSequences(Weights weights, char *seq1, char *seq2,
-		int size_seq2) {
-	const char *cons_Groups[9] = { "NDEQ", "MILV", "FYM", "NEQK", "QHRK", "HY",
-			"STA", "NHQK", "MILF" };
-	const char *semi_Cons_Groups[11] = { "SAG", "SGND", "NEQHRK", "ATV", "STPA",
-			"NDEQHK", "HFY", "CSA", "STNK", "SNDEQK", "FVLIM" };
-	int i;
-	Counts counts;
-	double score = 0.0;
-	counts.countStars = 0;
-	counts.countDots = 0;
-	counts.countColons = 0;
-	counts.countSpaces = 0;
-	score = (weights.w1 * (double) counts.countStars)
-			- (weights.w2 * (double) counts.countColons)
-			- (weights.w3 * (double) counts.countDots)
-			- (weights.w4 * (double) counts.countSpaces);
-	return score;
 }
 
 double compareSameLenSequencesNew(char *seq1, char *seq2, int size_seq2,
@@ -341,6 +261,7 @@ double compareSameLenSequencesNew(char *seq1, char *seq2, int size_seq2,
 	weights.w3 = 1;
 	weights.w4 = 1;
 	int current_offset = offset;
+	printf("\n\n");
 	for (i = 0; i < size_seq2; i++) {
 		current_offset = i + offset;
 		printf("Long:%c Short:%c\n", seq1[current_offset], seq2[i]);
@@ -356,10 +277,13 @@ double compareSameLenSequencesNew(char *seq1, char *seq2, int size_seq2,
 			counts.countSpaces++;
 		}
 	}
-	score = (weights.w1 * (double) counts.countStars)
-			- (weights.w2 * (double) counts.countColons)
-			- (weights.w3 * (double) counts.countDots)
-			- (weights.w4 * (double) counts.countSpaces);
+	printf("w1=%1.3f | w2=%1.3f | w3=%1.3f | w4=%1.3f\n", weights.w1,
+			weights.w2, weights.w3, weights.w4);
+	printf("starts:%d | colons:%d | dots:%d | spaces:%d\n", counts.countStars,
+			counts.countColons, counts.countDots, counts.countSpaces);
+	score = (weights.w1 * counts.countStars) - (weights.w2 * counts.countColons)
+			- (weights.w3 * counts.countDots)
+			- (weights.w4 * counts.countSpaces);
 	return score;
 }
 
@@ -385,135 +309,6 @@ void createWeightType(MPI_Datatype *weightMpiType) {
 			weightMpiType);
 	MPI_Type_commit(weightMpiType);
 }
-//
-//void master(char **seq1, char **seq2, char **procedure, int *_seq1, int *size_seq2) {
-//	int i;
-//	int size;
-//	MPI_Datatype weightMPIType;
-//	createWeightType(&weightMPIType);
-//	int size_seq2;
-//	int size_seq1;
-//	MPI_Status status;
-//	MPI_Datatype scoreMPIType;
-//	createScoreType(&scoreMPIType);
-//	Scores scores;
-//	Weights weights;
-//	Counts counts;
-//	//MPI_Comm_size(MPI_COMM_WORLD, &size);
-//	//readFromFile("input.txt", &weights, seq1, numOfSeq2, seq2);
-//	readFromFile("input.txt", &weights, seq1, seq2, procedure);
-//	printf("%s\n", *seq2);
-//	size_seq2 = getSize1(*seq2);
-//	compareSequence(*seq1, *seq2, &counts, weights, &scores);
-//	printf("%s\n", *seq2);
-//
-//	//compareSequence()''
-//	//addMutant(*seq1,*seq2,size_seq2) ;
-//	/*
-//	 printf("%s\n",*seq1);
-//	 printf("%s\n",*seq2);
-//	 printf("%s\n",*procedure);
-//	 */
-//	//printf("%d\n",size);
-//	//printf("%d\n",calcMaxOffsetForEachSeq(*seq1,*seq2));
-//	//readFromFile("input.txt", &weights, seq1, seq2, procedure);
-//	size_seq1 = getSize1(*seq1);
-//	//printf(size_seq1);
-//	int task_count = 0;
-//	int term_count = 0;
-//	int TERMINATION_TAG = 1;
-//	MPI_Bcast(&size_seq1, 1, MPI_INT, 0, MPI_COMM_WORLD);
-//	MPI_Bcast(*seq1, size_seq1, MPI_CHAR, 0, MPI_COMM_WORLD);
-//	MPI_Bcast(&weights, 1, weightMPIType, 0, MPI_COMM_WORLD);
-//	//MPI_Bcast(numOfSeq2, 1, MPI_INT, 0, MPI_COMM_WORLD);
-//	const char *file_path;
-//	file_path = "output.txt";
-//	FILE *file = fopen(file_path, "w");
-//	//int num_seq;
-//	writeToFile(file, &scores, *seq2);
-//
-//	Scores *scores_arr;
-//	//scores_arr = (Scores*) malloc(*numOfSeq2 * sizeof(Scores));
-//	/*
-//	 if (size - 1 == *numOfSeq2) {
-//	 for (i = 1; i < size; i++) {
-//	 size_seq2 = getSize1((*seq2)[i - 1]);
-//	 MPI_Send(&size_seq2, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
-//	 MPI_Send(&i, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
-//	 MPI_Send((*seq2)[i - 1], size_seq2, MPI_CHAR, i, 0, MPI_COMM_WORLD);
-//	 }
-//
-//	 for (i = 1; i < size; i++) {
-//	 MPI_Recv(&num_seq, 1, MPI_INT, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD,
-//	 &status);
-//	 MPI_Recv(&scores, 1, scoreMPIType, MPI_ANY_SOURCE, 0,
-//	 MPI_COMM_WORLD, &status);
-//	 scores.num_of_Seq = num_seq;
-//	 scores_arr[num_seq - 1] = scores;
-//	 MPI_Send(&term_count, 1, MPI_INT, status.MPI_SOURCE,
-//	 TERMINATION_TAG, MPI_COMM_WORLD);
-//	 }
-//	 } else if (*numOfSeq2 > size - 1) {
-//	 for (i = 1; i < size; i++) {
-//	 size_seq2 = getSize1((*seq2)[i - 1]);
-//	 MPI_Send(&size_seq2, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
-//	 task_count++;
-//	 MPI_Send(&task_count, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
-//	 MPI_Send((*seq2)[i - 1], size_seq2, MPI_CHAR, i, 0, MPI_COMM_WORLD);
-//
-//	 }
-//	 do {
-//	 MPI_Recv(&num_seq, 1, MPI_INT, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD,
-//	 &status);
-//	 MPI_Recv(&scores, 1, scoreMPIType, MPI_ANY_SOURCE, 0,
-//	 MPI_COMM_WORLD, &status);
-//	 scores.num_of_Seq = num_seq;
-//	 scores_arr[num_seq - 1] = scores;
-//	 if (task_count < *numOfSeq2) {
-//	 size_seq2 = getSize1((*seq2)[task_count]);
-//	 task_count++;
-//	 MPI_Send(&size_seq2, 1, MPI_INT, status.MPI_SOURCE, 0,
-//	 MPI_COMM_WORLD);
-//	 MPI_Send(&task_count, 1, MPI_INT, status.MPI_SOURCE, 0,
-//	 MPI_COMM_WORLD);
-//	 MPI_Send((*seq2)[task_count - 1], size_seq2, MPI_CHAR,
-//	 status.MPI_SOURCE, 0, MPI_COMM_WORLD);
-//
-//	 } else {
-//	 MPI_Send(&term_count, 1, MPI_INT, status.MPI_SOURCE,
-//	 TERMINATION_TAG, MPI_COMM_WORLD);
-//	 term_count++;
-//	 }
-//	 } while (term_count < size - 1);
-//	 } else {
-//	 for (i = 1; i < size; i++) {
-//	 if (i > *numOfSeq2) {
-//	 MPI_Send(&term_count, 1, MPI_INT, i, TERMINATION_TAG,
-//	 MPI_COMM_WORLD);
-//	 } else {
-//	 size_seq2 = getSize1((*seq2)[i - 1]);
-//	 MPI_Send(&size_seq2, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
-//	 MPI_Send(&i, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
-//	 MPI_Send((*seq2)[i - 1], size_seq2, MPI_CHAR, i, 0,
-//	 MPI_COMM_WORLD);
-//	 }
-//	 }
-//	 for (i = 1; i < *numOfSeq2 + 1; i++) {
-//	 MPI_Recv(&num_seq, 1, MPI_INT, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD,
-//	 &status);
-//	 MPI_Recv(&scores, 1, scoreMPIType, MPI_ANY_SOURCE, 0,
-//	 MPI_COMM_WORLD, &status);
-//	 scores.num_of_Seq = num_seq;
-//	 scores_arr[num_seq - 1] = scores;
-//	 MPI_Send(&term_count, 1, MPI_INT, status.MPI_SOURCE,
-//	 TERMINATION_TAG, MPI_COMM_WORLD);
-//	 }
-//	 }
-//	 writeToFile(file, scores_arr, *numOfSeq2);
-//	 */
-//	fclose(file);
-//
-//}
 
 void createScoreType(MPI_Datatype *scoreMpiType) {
 	int blocklengths[SCORE_NUM_ATTRIBUTES] = SCORE_BLOCK_LENGTH;
@@ -526,50 +321,6 @@ void createScoreType(MPI_Datatype *scoreMpiType) {
 	MPI_Type_create_struct(SCORE_NUM_ATTRIBUTES, blocklengths, offsets, types,
 			scoreMpiType);
 	MPI_Type_commit(scoreMpiType);
-}
-
-void slave(char *one_seq, char *seq1) {
-	int dest;
-	dest = 0;
-	int my_rank;
-	int size_seq1;
-	int numOfSeq2 = 0;
-	Scores scores;
-	Weights weights;
-	Counts counts;
-	MPI_Datatype scoreMPIType;
-	createScoreType(&scoreMPIType);
-	MPI_Datatype weightMPIType;
-	createWeightType(&weightMPIType);
-	scores.mute_loc = 0;
-	scores.score = 0.0;
-	scores.offset = 0;
-	int termination_tag = 1;
-	MPI_Status status;
-	int tag = 0;
-	int size;
-	int num_seq;
-	MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
-	MPI_Bcast(&size_seq1, 1, MPI_INT, 0, MPI_COMM_WORLD);
-	seq1 = (char*) calloc(size_seq1, sizeof(char));
-	MPI_Bcast(seq1, size_seq1, MPI_CHAR, 0, MPI_COMM_WORLD);
-	MPI_Bcast(&weights, 1, weightMPIType, 0, MPI_COMM_WORLD);
-	MPI_Bcast(&numOfSeq2, 1, MPI_INT, 0, MPI_COMM_WORLD);
-	do {
-		MPI_Recv(&size, 1, MPI_INT, dest, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
-		if (status.MPI_TAG == termination_tag) {
-			return;
-		}
-		MPI_Recv(&num_seq, 1, MPI_INT, dest, MPI_ANY_TAG, MPI_COMM_WORLD,
-				&status);
-		one_seq = (char*) calloc(size, sizeof(char));
-		MPI_Recv(one_seq, size, MPI_CHAR, dest, tag, MPI_COMM_WORLD, &status);
-		if (my_rank % 2 == 0) {
-			compareSequence(seq1, one_seq, &counts, weights, &scores);
-		}
-		MPI_Send(&num_seq, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
-		MPI_Send(&scores, 1, scoreMPIType, 0, 0, MPI_COMM_WORLD);
-	} while (1);
 }
 
 int main(int argc, char *argv[]) {
@@ -608,11 +359,6 @@ int main(int argc, char *argv[]) {
 	return 0;
 }
 
-///tis is new
-int new_main(int argc, char *argv[]) {
-
-}
-
 void new_master(char **seq1, char **seq2, char **procedure) {
 	int i;
 	int size;
@@ -630,12 +376,13 @@ void new_master(char **seq1, char **seq2, char **procedure) {
 	Weights weights;
 	Counts counts;
 
-	readFromFile("input.txt", &weights, seq1, seq2, procedure, &size_seq1, &size_seq2);
+	readFromFile("input.txt", &weights, seq1, seq2, procedure, &size_seq1,
+			&size_seq2);
 	int p;
 	MPI_Comm_size(MPI_COMM_WORLD, &p);
 
 	int maxOffset = calcMaxOffsetForEachSeq(*seq1, *seq2);
-	batch = maxOffset / (p - 1);
+	batch = ceil(maxOffset / (p - 1));
 
 	int task_count = 0;
 	int term_count = 0;
@@ -719,12 +466,13 @@ void new_slave(char *seq1, char *seq2) {
 
 #pragma omp single
 		{
-			for (offset = (my_rank - 1) * batch; offset < my_rank * batch;
+			for (offset = (my_rank - 1) * batch; offset <= my_rank * batch;
 					offset++) {
 				int d = offset; //dont touch!
 #pragma omp task
 				{
-					checkOneCase(my_rank, batch, d, scores_each_t, seq1, seq2, size_seq2);
+					checkOneCase(my_rank, batch, d, scores_each_t, seq1, seq2,
+							size_seq2);
 				}
 			}
 
@@ -745,8 +493,11 @@ void new_slave(char *seq1, char *seq2) {
 
 }
 
-void checkOneCase(int my_rank, int batch, int x, Scores *scores_each_t, char *seq1, char *seq2, int size_seq2) {
-	scores_each_t[x % batch].score = compareSameLenSequencesNew(seq1, seq2, size_seq2, x);
+void checkOneCase(int my_rank, int batch, int x, Scores *scores_each_t,
+		char *seq1, char *seq2, int size_seq2) {
+	double score = compareSameLenSequencesNew(seq1, seq2, size_seq2, x);
+	printf("SCORE: %1.3f\n", score);
+	scores_each_t[x % batch].score = score;
 	scores_each_t[x % batch].offset = x;
 }
 
